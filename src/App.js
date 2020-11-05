cf0a4fa06da9eafcddb8bd436371cde13f791fc2
Hamza Ben Abdallah <hamza.ben.abdallah02@gmail.com>
2020-11-05 14:34:02 -0500

    Clean some routing code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/HomePage';
 
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
-
 function App() {
     return (
         <div>
-            <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route exact path='/hats' component={HatsPage} />
-            </Switch>
+            <Route exact path='/' component={HomePage} />
         </div>
     );
 }
