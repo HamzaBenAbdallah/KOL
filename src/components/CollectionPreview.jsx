df74b2d4d5642f41fef8e8e3ae86baecca0bc604
Hamza Ben Abdallah <hamza.ben.abdallah02@gmail.com>
2020-11-05 15:25:37 -0500

    Add the CollectionItem component

--- src/components/CollectionPreview.jsx
+++ src/components/CollectionPreview.jsx
@@ -1,14 +1,16 @@
+import CollectionItem from './CollectionItem';
+
 import './CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => (
-    <div className='collection'>
+    <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
                 // only render the first 4 items from the array
                 .filter((item, index) => index < 4)
-                .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))}
         </div>
     </div>
